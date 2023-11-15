93f2e453af7c3d9c81dd6160462a55f1b8a5be28
tjohora1 <tjohora@gmail.com>
2023-11-15 13:03:59 +0000

    Add Create Category Validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -22,5 +22,21 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -10,8 +10,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "The field Display Order must be between 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
